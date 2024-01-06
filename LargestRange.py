875f44b556f61e97340041be16deed29c1ee0553
Jared Cazel <jjcazel@gmail.com>
2024-01-05 20:38:41 -0500

    Create LargestRange.py

---
+++ LargestRange.py
@@ -0,0 +1,27 @@
+//O(n) time | O(n) space where n is the number of unique numbers in the input array
+export function largestRange(array: number[]): [number, number] {
+  const numsSet = new Set(array);
+  let minNum = Math.min(...numsSet); // 0
+  let maxNum = Math.max(...numsSet);
+  let greatestRangeEdges: [number, number] = [minNum, minNum]; // [0, 0]
+  let currentRangeEdges: [number, number] = [minNum, minNum]; // [0, 0]
+  let greatestRange = 0; // 0
+
+  for (let num = minNum; num <= maxNum; num++) { // num = 1
+    console.log('num', num, 'minNum', minNum, 'maxNum', maxNum);
+    if (!numsSet.has(num)) {
+      currentRangeEdges[0] = num + 1;
+      continue;
+    }
+    
+    currentRangeEdges[1] = num;
+    numsSet.delete(num);
+
+    if (currentRangeEdges[1] - currentRangeEdges[0] > greatestRange) {
+      greatestRange = currentRangeEdges[1] - currentRangeEdges[0];
+      greatestRangeEdges = [...currentRangeEdges];
+    }
+  }
+
+  return greatestRangeEdges;
+}
